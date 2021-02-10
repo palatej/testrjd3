45311665fc407945e374153ffc9782ca602410be
palatej <jean.palate@nbb.be>
2021-02-10 11:33:43 +0100

    Starting messages

--- R/test_tramoseats.R
+++ R/test_tramoseats.R
@@ -1,5 +1,5 @@
-library(RJDemetra)
-suppressPackageStartupMessages(library(RJDemetra3))
+suppressWarnings(library(RJDemetra))
+suppressWarnings(library(RJDemetra3))
 
 load("./Data/retail.rda")
 
@@ -15,8 +15,8 @@ s3<-RJDemetra3::spec_tramoseats_default(spec)
 s3$tramo$estimate$tol<-1e-7
 
 t0<-Sys.time()
-#ts3_all<-lapply(retail, function(z){RJDemetra3::tramoseats(z,spec)})
-ts3_all<-lapply(retail, function(z){RJDemetra3::tramoseats(z,s3)})
+ts3_all<-lapply(retail, function(z){RJDemetra3::tramoseats(z,spec)})
+#ts3_all<-lapply(retail, function(z){RJDemetra3::tramoseats(z,s3)})
 t1<-Sys.time()
 cat("\nRJD3:\n")
 cat(t1-t0)
@@ -27,3 +27,7 @@ sa3<-lapply(ts3_all, function(z){z$final$sa$data})
 
 del<-lapply(1:length(ts_all), function(i){summary(abs(sa[[i]]-sa3[[i]])/sa[[i]])})
 plot(sapply(del, function(x){log10(x[5])}))
+
+q<-t(sapply(ts3_all, function(z){return (as.array(z$diagnostics$vardecomposition))}))
+plot(unlist(q[,1]), unlist(q[,2]) )
+plot(unlist(q[,4]), type='h' )
