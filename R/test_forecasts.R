e0d05c48fed6647b6df71c39b71cc338a846a4fd
Palate Jean <jean.palate@nbb.be>
2023-04-06 11:49:27 +0200

    Naming convention

--- R/test_forecasts.R
+++ R/test_forecasts.R
@@ -1,8 +1,8 @@
 load("./Data/spain.rda")
 
 
-sf<-rjd3sts::sts.forecast(spain$IPI_GEN, model="none", nf=12)
-tf<-rjd3tramoseats::tramo.forecast(spain$IPI_GEN, spec="tr0", nf=12)
+sf<-rjd3sts::sts_forecast(spain$IPI_GEN, model="none", nf=12)
+tf<-rjd3tramoseats::tramo_forecast(spain$IPI_GEN, spec="tr0", nf=12)
 
 plot(sf[,1], type="l")
 lines(as.numeric(tf[,1]), col="red")
