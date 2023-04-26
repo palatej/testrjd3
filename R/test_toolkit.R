fd4ce92dfc623b847a20804ddda2c2cbc0fdfb9e
Palate Jean <jean.palate@nbb.be>
2023-04-26 16:44:41 +0200

    Adaptation to the new API

--- R/test_toolkit.R
+++ R/test_toolkit.R
@@ -1,11 +1,11 @@
 suppressPackageStartupMessages(library(rjdemetra3))
 
 
-a<-rjd3toolkit::randomsT(50, 1000)
+a<-rjd3toolkit::randoms_t (50, 1000)
 
 print(rjd3toolkit::bowmanshenton(a) )
 print(rjd3toolkit::doornikhansen(a) )
 print(rjd3toolkit::ljungbox(a, k=36) )
 print(rjd3toolkit::autocorrelations(a) )
-print(rjd3toolkit::autocorrelations.inverse(a) )
-print(rjd3toolkit::autocorrelations.partial(a) )
+print(rjd3toolkit::autocorrelations_inverse(a) )
+print(rjd3toolkit::autocorrelations_partial(a) )
